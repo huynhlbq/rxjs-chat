591f1a6a0a918b20d45fd092c7917b0178136f9d
Le Ba Quoc Huynh <huynh.le@savvycom-software.com>
2018-07-27 17:55:06 +0700

    Add comment

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -91,6 +91,9 @@ export class MessagesService {
     this.newMessages.next(message);
   }
 
+  // messagesForThreadUser takes a Thread and a User and returns a new stream of Messages that are
+  // filtered on that Thread and not authored by the User. That is, it is a stream of “everyone else’s”
+  // messages in this Thread.
   messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
     return this.newMessages
       .filter((message: Message) => {
--- src/app/user/users.service.ts
+++ src/app/user/users.service.ts
@@ -9,6 +9,11 @@ import { User } from './user.model';
 @Injectable()
 export class UsersService {
   // `currentUser` contains the current user
+  // You can think of a Subject as a “read/write” stream
+  // messages are published immediately, a new subscriber risks missing the latest value of the stream.
+  // BehaviourSubject stores the last value. Meaning that any subscriber to the stream will receive the latest value.
+  // This is great for us because it means that any part of our application can subscribe to the
+  // UsersService.currentUser stream and immediately know who the current user is
   currentUser: Subject<User> = new BehaviorSubject<User>(null);
 
   public setCurrentUser(newUser: User): void {
